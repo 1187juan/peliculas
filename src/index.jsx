70126e436d315a3da72f2d84281d195d5baca6de
Juan <1187juan@gmail.com>
2022-04-10 20:18:01 -0500

    pwa settings

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
@@ -10,3 +11,13 @@ root.render(
 		<App />
 	</React.StrictMode>
 )
+
+serviceWorkerRegistration.register({
+	onUpdate: async (registration) => {
+		if (registration && registration.waiting) {
+			await registration.unregister()
+			registration.waiting.postMessage({ type: 'SKIP_WAITING' })
+			window.location.reload()
+		}
+	},
+})
